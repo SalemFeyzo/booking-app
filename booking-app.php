ca9c2c4fa9fda53d3800d51213c4e3ea0d7f649b
salemFeyzo <f1989salim@gmail.com>
2022-05-29 19:46:03 +0300

    Preppaer the database

--- booking-app.php
+++ booking-app.php
@@ -2,7 +2,7 @@
 
 /**
  * Plugin Name:       Booking App
- * Description:       Booking app made especially for amphaul.com.
+ * Description:       This plugin was made exclusively for amphaul.com.
  * Requires at least: 5.8
  * Requires PHP:      7.0
  * Version:           0.1.0
@@ -25,6 +25,15 @@ class BookingApp
 		add_shortcode('booking-app', array($this, 'frontend_shortcode'));
 		add_action('admin_enqueue_scripts', array($this, 'enqueue_admin'));
 		add_action('admin_menu', array($this, 'admin_menu'));
+		register_activation_hook(__FILE__, array($this, 'create_services_table'));
+		register_activation_hook(__FILE__, array($this, 'create_items_table'));
+		register_activation_hook(__FILE__, array($this, 'create_dismantling_table'));
+		register_activation_hook(__FILE__, array($this, 'create_vehicles_table'));
+		register_activation_hook(__FILE__, array($this, 'create_stairs_table'));
+		register_activation_hook(__FILE__, array($this, 'create_service_addresses_table'));
+		register_activation_hook(__FILE__, array($this, 'create_service_items_table'));
+		register_activation_hook(__FILE__, array($this, 'create_orders_table'));
+		register_activation_hook(__FILE__, array($this, 'create_order_items_table'));
 	}
 
 	function onInit()
@@ -100,6 +109,199 @@ class BookingApp
 
 <?php
 
+	}
+
+
+	function create_services_table()
+	{
+		global $wpdb;
+		$charset_collate = $wpdb->get_charset_collate();
+		$table_name = $wpdb->prefix . 'booking_app_services';
+		require_once ABSPATH . "wp-content/plugins/booking-app/sql/services_sql.php";
+		$query = $wpdb->prepare('SHOW TABLES LIKE %s', $wpdb->esc_like($table_name));
+		//checking if the table exists if not go ahead and create it & insert the default data
+		if (!$wpdb->get_var($query) == $table_name) {
+			require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+			dbDelta($sql);
+			//Adding default data
+			require_once ABSPATH . "wp-content/plugins/booking-app/dummy_data/services.php";
+			foreach ($services as
+				$key => $service) {
+				$wpdb->insert(
+					$table_name,
+					$service,
+				);
+			}
+		}
+	}
+
+	function create_service_addresses_table()
+	{
+		global $wpdb;
+		$charset_collate = $wpdb->get_charset_collate();
+		$table_name = $wpdb->prefix . 'booking_app_service_addresses';
+		$service_table_name = $wpdb->prefix . 'booking_app_services';
+		require_once ABSPATH . "wp-content/plugins/booking-app/sql/service_addresses_sql.php";
+		$query = $wpdb->prepare('SHOW TABLES LIKE %s', $wpdb->esc_like($table_name));
+		//checking if the table exists if not go ahead and create it & insert the default data
+		if (!$wpdb->get_var($query) == $table_name) {
+			require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+			dbDelta($sql);
+		}
+	}
+
+	function create_orders_table()
+	{
+		global $wpdb;
+		$charset_collate = $wpdb->get_charset_collate();
+		$table_name = $wpdb->prefix . 'booking_app_orders';
+		$service_table_name = $wpdb->prefix . 'booking_app_services';
+		require_once ABSPATH . "wp-content/plugins/booking-app/sql/orders_sql.php";
+		$query = $wpdb->prepare('SHOW TABLES LIKE %s', $wpdb->esc_like($table_name));
+		//checking if the table exists if not go ahead and create it & insert the default data
+		if (!$wpdb->get_var($query) == $table_name) {
+			require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+			dbDelta($sql);
+			//Adding default data
+			require_once ABSPATH . "wp-content/plugins/booking-app/dummy_data/demo_order.php";
+
+			$wpdb->insert(
+				$table_name,
+				$demo_order,
+			);
+		}
+	}
+
+	function create_items_table()
+	{
+		global $wpdb;
+		$charset_collate = $wpdb->get_charset_collate();
+		$table_name = $wpdb->prefix . 'booking_app_items';
+		require_once ABSPATH . "wp-content/plugins/booking-app/sql/items_sql.php";
+		$query = $wpdb->prepare('SHOW TABLES LIKE %s', $wpdb->esc_like($table_name));
+		//checking if the table exists if not go ahead and create it & insert the default data
+		if (!$wpdb->get_var($query) == $table_name) {
+			require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+			dbDelta($sql);
+			//Adding default data
+			require_once ABSPATH . "wp-content/plugins/booking-app/dummy_data/items.php";
+			foreach ($items as
+				$key => $item) {
+				$wpdb->insert(
+					$table_name,
+					$item,
+				);
+			}
+		}
+	}
+
+	function create_dismantling_table()
+	{
+		global $wpdb;
+		$charset_collate = $wpdb->get_charset_collate();
+		$table_name = $wpdb->prefix . 'booking_app_dismantling';
+		require_once ABSPATH . "wp-content/plugins/booking-app/sql/dismantling_sql.php";
+		$query = $wpdb->prepare('SHOW TABLES LIKE %s', $wpdb->esc_like($table_name));
+		//checking if the table exists if not go ahead and create it & insert the default data
+		if (!$wpdb->get_var($query) == $table_name) {
+			require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+			dbDelta($sql);
+			require_once ABSPATH . "wp-content/plugins/booking-app/dummy_data/dismantling.php";
+			$wpdb->insert(
+				$table_name,
+				$dismantling,
+			);
+		}
+	}
+	function create_vehicles_table()
+	{
+		global $wpdb;
+		$charset_collate = $wpdb->get_charset_collate();
+		$table_name = $wpdb->prefix . 'booking_app_vehicles';
+		require_once ABSPATH . "wp-content/plugins/booking-app/sql/vehicles_sql.php";
+		$query = $wpdb->prepare('SHOW TABLES LIKE %s', $wpdb->esc_like($table_name));
+		//checking if the table exists if not go ahead and create it & insert the default data
+		if (!$wpdb->get_var($query) == $table_name) {
+			require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+			dbDelta($sql);
+			//Adding default data
+			require_once ABSPATH . "wp-content/plugins/booking-app/dummy_data/vehicles.php";
+			foreach ($vehicles as
+				$key => $vehicle) {
+				$wpdb->insert(
+					$table_name,
+					$vehicle,
+				);
+			}
+		}
+	}
+	function create_stairs_table()
+	{
+		global $wpdb;
+		$charset_collate = $wpdb->get_charset_collate();
+		$table_name = $wpdb->prefix . 'booking_app_stairs';
+		require_once ABSPATH . "wp-content/plugins/booking-app/sql/stairs_sql.php";
+		$query = $wpdb->prepare('SHOW TABLES LIKE %s', $wpdb->esc_like($table_name));
+		//checking if the table exists if not go ahead and create it & insert the default data
+		if (!$wpdb->get_var($query) == $table_name) {
+			require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+			dbDelta($sql);
+			require_once ABSPATH . "wp-content/plugins/booking-app/dummy_data/stairs.php";
+			$wpdb->insert(
+				$table_name,
+				$stairs,
+			);
+		}
+	}
+
+	function create_service_items_table()
+	{
+		global $wpdb;
+		$charset_collate = $wpdb->get_charset_collate();
+		$table_name = $wpdb->prefix . 'booking_app_service_items';
+		$items_table_name = $wpdb->prefix . 'booking_app_items';
+		$service_table_name = $wpdb->prefix . 'booking_app_services';
+		require_once ABSPATH . "wp-content/plugins/booking-app/sql/service_items_sql.php";
+		$query = $wpdb->prepare('SHOW TABLES LIKE %s', $wpdb->esc_like($table_name));
+		//checking if the table exists if not go ahead and create it & insert the default data
+		if (!$wpdb->get_var($query) == $table_name) {
+			require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+			dbDelta($sql);
+			//Adding default data
+			require_once ABSPATH . "wp-content/plugins/booking-app/dummy_data/service_items.php";
+			foreach ($service_items as
+				$key => $service_item) {
+				$wpdb->insert(
+					$table_name,
+					$service_item,
+				);
+			}
+		}
+	}
+
+	function create_order_items_table()
+	{
+		global $wpdb;
+		$charset_collate = $wpdb->get_charset_collate();
+		$table_name = $wpdb->prefix . 'booking_app_order_items';
+		$items_table_name = $wpdb->prefix . 'booking_app_items';
+		$order_table_name = $wpdb->prefix . 'booking_app_orders';
+		require_once ABSPATH . "wp-content/plugins/booking-app/sql/order_items_sql.php";
+		$query = $wpdb->prepare('SHOW TABLES LIKE %s', $wpdb->esc_like($table_name));
+		//checking if the table exists if not go ahead and create it & insert the default data
+		if (!$wpdb->get_var($query) == $table_name) {
+			require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+			dbDelta($sql);
+			//Adding default data
+			require_once ABSPATH . "wp-content/plugins/booking-app/dummy_data/order_items.php";
+			foreach ($order_items as
+				$key => $order_item) {
+				$wpdb->insert(
+					$table_name,
+					$order_item,
+				);
+			}
+		}
 	}
 }
 
